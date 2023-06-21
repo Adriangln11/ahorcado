ca64370db32f566aab021d6768cd0a7332be7fa1
AdrianDiaz <cesarqqu@gmail.com>
2023-06-20 23:52:14 -0600

    html interface

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
+import http.server
+import socketserver
 import random
 
-# Lista de palabras para el juego
 words = ['javascript', 'backend', 'github', 'python', 'web', 'codigo']
 
-# Función para seleccionar una palabra al azar
 def select_word():
     return random.choice(words)
 
-# Función para mostrar el tablero del juego
 def show_letter(hide_word, rounds):
-    print("Palabra: ", end="")
-    for letra in hide_word:
-        print(letra, end=" ")
-    print("\nIntentos restantes: ", rounds)
+    content = f"<p>Palabra: {hide_word}</p>"
+    content += f"<p>Intentos restantes: {rounds}</p>"
+    return content
 
-# Función para actualizar la palabra oculta con las letras adivinadas
 def update_hide_word(word, hide_word, letter):
     new_hide_word = ""
     for i in range(len(word)):
@@ -24,39 +21,112 @@ def update_hide_word(word, hide_word, letter):
             new_hide_word += hide_word[i]
     return new_hide_word
 
-# Función principal del juego
-def main():
-    word = select_word()
-    hide_word = "_" * len(word)
-    rounds = 6
-    correct_letters = []
+class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        super().end_headers()
 
-    print("Bienvenido al juego del ahorcado!")
+    def do_GET(self):
+        if self.path == '/':
+            word = select_word()
+            hide_word = "_" * len(word)
+            rounds = 6
+            correct_letters = []
 
-    while True:
-        show_letter(hide_word, rounds)
-
-        # Pedir al jugador que ingrese una letra
-        letter = input("Ingrese una letra: ").lower()
-
-        if letter.isalpha() and len(letter) == 1:
-            if letter in correct_letters:
-                print("Ya has adivinado esa letra. Intenta con otra.")
-                continue
-            elif letter in word:
-                correct_letters.append(letter)
-                hide_word = update_hide_word(word, hide_word, letter)
-                if "_" not in hide_word:
-                    print("¡Felicidades! Has adivinado la palabra:", word)
-                    break
-            else:
-                rounds -= 1
-                print("La letra no está en la palabra. Intentos restantes:", rounds)
-                if rounds == 0:
-                    print("¡Oh no! Te has quedado sin intentos. La palabra era:", word)
-                    break
+            response = """
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <link rel="stylesheet" href="https://bootswatch.com/5/darkly/bootstrap.min.css">
+                <title>Miniproyectos | Ahorcado</title>
+            </head>
+            <body class="container d-flex flex-column align-items-center">
+            """
+            response += show_letter(hide_word, rounds)
+            response += """
+                <form class="d-flex flex-column" method="POST" action="/guess_letter">
+                    <label>Ingrese una letra:</label>
+                    <input type="text" name="letter" required><br>
+                    <input type="hidden" name="word" value="{}">
+                    <input type="hidden" name="hide_word" value="{}">
+                    <input type="hidden" name="rounds" value="{}">
+                    <input type="hidden" name="correct_letters" value="{}">
+                    <input type="submit" value="Adivinar">
+                </form>
+            </body>
+            </html>
+            """.format(word, hide_word, rounds, correct_letters)
+            
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(response.encode())
         else:
-            print("Por favor, ingresa una sola letra.")
+            super().do_GET()
 
-# Iniciar el juego
-main()
+    def do_POST(self):
+        if self.path == '/guess_letter':
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length).decode('utf-8')
+            
+            letter = post_data.split('&')[0].split('=')[1]
+            word = post_data.split('&')[1].split('=')[1]
+            hide_word = post_data.split('&')[2].split('=')[1]
+            rounds = int(post_data.split('&')[3].split('=')[1])
+            correct_letters = post_data.split('&')[4].split('=')[1].split(',')
+
+            if letter.isalpha() and len(letter) == 1:
+                if letter in correct_letters:
+                    response = "<p>Ya has adivinado esa letra. Intenta con otra.</p>"
+                elif letter in word:
+                    correct_letters.append(letter)
+                    hide_word = update_hide_word(word, hide_word, letter)
+                    if "_" not in hide_word:
+                        response = "<p>¡Felicidades! Has adivinado la palabra: {}</p>".format(word)
+                    else:
+                        response = show_letter(hide_word, rounds)
+                else:
+                    rounds -= 1
+                    response = "<p>La letra no está en la palabra. Intentos restantes: {}</p>".format(rounds)
+                    if rounds == 0:
+                        response = "<p>¡Oh no! Te has quedado sin intentos. La palabra era: {}</p>".format(word)
+            else:
+                response = "<p>Por favor, ingresa una sola letra.</p>"
+            
+            response += """
+                <html>
+                    <head>
+                        <meta charset="UTF-8">
+                        <link rel="stylesheet" href="https://bootswatch.com/5/darkly/bootstrap.min.css">
+                        <title>Miniproyectos | Ahorcado</title>
+                    </head>
+                    <body class="container d-flex flex-column align-items-center">
+                        <form class="d-flex flex-column" method="POST" action="/guess_letter">
+                            <label>Ingrese una letra:</label>
+                            <input type="text" name="letter" required><br>
+                            <input type="hidden" name="word" value="{}">
+                            <input type="hidden" name="hide_word" value="{}">
+                            <input type="hidden" name="rounds" value="{}">
+                            <input type="hidden" name="correct_letters" value="{}">
+                            <input type="submit" value="Adivinar">
+                        </form>
+                    </body>
+                </html>
+            """.format(word, hide_word, rounds, ','.join(correct_letters))
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(response.encode())
+        else:
+            super().do_POST()
+
+def main():
+    PORT = 8000
+
+    with socketserver.TCPServer(("", PORT), MyRequestHandler) as httpd:
+        print("Servidor en ejecución en el puerto", PORT)
+        httpd.serve_forever()
+
+if __name__ == "__main__":
+    main()
